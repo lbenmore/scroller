0b1545df14887e894c6e5aca3dfe8bba37f9b177
Lisa Benmore <lbenmore@gmail.com>
2021-10-11 16:03:55 -0700

    added content for first demo page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 import Scroller from './scroller/scroller.js';
 
 ;(function (win, doc) {
+  function updateStyling () {
+    document.body.style.setProperty('--vh', `${win.innerHeight / 100}px`);
+  }
+  
+  function eventListeners () {
+    addEventListener('resize', updateStyling);
+  }
+  
   function initFns () {
+    updateStyling();
+    eventListeners();
+    
     fetch('./scroller_config.json')
       .then(res => res.json())
-      .then(json => new Scroller({ ...json, debug: 1 }))
+      .then(json => new Scroller({ ...json, debug: 0 }))
       .catch(err => console.log(err));
   }
 
